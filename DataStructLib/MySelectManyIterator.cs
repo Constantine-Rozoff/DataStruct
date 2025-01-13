60adef48eef8c61f32153a9b020d82182d524102
Kostiantyn-Rozov <konstantin@monsterlg.com>
2025-01-14 00:08:57 +0200

    Final fix of SelectMany iterator

--- DataStructLib/MySelectManyIterator.cs
+++ DataStructLib/MySelectManyIterator.cs
@@ -7,32 +7,32 @@ public class MySelectManyIterator<T, TResult> : IEnumerator<TResult>
     private readonly IEnumerator<T> collection;
     private readonly Func<T, IEnumerable<TResult>> selector;
     private IEnumerator<TResult> currentCollection;
-    public TResult Current { get; private set; }
-    object? IEnumerator.Current => collection.Current;
+    public TResult Current => currentCollection.Current;
+    object? IEnumerator.Current => Current;
 
     public MySelectManyIterator(IEnumerator<T> collection, Func<T, IEnumerable<TResult>> selector)
     {
         this.collection = collection;
         this.selector = selector;
     }
-    
+
     public bool MoveNext()
     {
-        if (currentCollection == null!)
+        l2: if (currentCollection == null!)
         {
             l1: var result = collection.MoveNext();
             if (!result) return false;
 
             var current = collection.Current;
             currentCollection = selector(current).GetEnumerator();
-            
+
             var result2 = currentCollection.MoveNext();
             if (!result2)
             {
                 currentCollection = null;
                 goto l1;
             }
-            
+
             return true;
         }
         else
@@ -41,6 +41,7 @@ public class MySelectManyIterator<T, TResult> : IEnumerator<TResult>
             if (!result3)
             {
                 currentCollection = null;
+                goto l2;
             }
 
             return true;
