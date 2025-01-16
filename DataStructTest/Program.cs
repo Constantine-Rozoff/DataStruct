f7e19461e25bfcfacdced4fddecd6924f7bd3020
Kostiantyn-Rozov <konstantin@monsterlg.com>
2025-01-16 21:29:09 +0200

    Linq examples 2

--- DataStructTest/Program.cs
+++ DataStructTest/Program.cs
@@ -5,6 +5,26 @@ namespace DataStructTest;
 
 class Program
 {
+    class Actor {
+        public string Name { get; set; }
+        public DateTime Birthdate {get; set; }
+    }
+    
+    abstract class ArtObject {
+        public string Author { get; set;}
+        public string Name { get; set;}
+        public int Year { get; set;}
+    }
+
+    class Film : ArtObject {
+        public int Length { get; set;}
+        public IEnumerable<Actor> Actors { get; set;}
+    }
+
+    class Book : ArtObject {
+        public int Pages { get; set; }
+    }
+    
     //Draft area. It's able to show a particular output of the methods
     static void Main(string[] args)
     {
@@ -47,5 +67,146 @@ class Program
         
         //#10
         Console.WriteLine(string.Join(", ", str4.Split(';').Where(s => s.StartsWith("aa")).Where(s => s.Contains("bb")).Select(s => s.ToString())));
+
+        Console.WriteLine("-----------------------------");
+        
+        //linq#2
+        
+        var data = new List<object>()
+        {
+            "Hello",
+            new Book() { Author = "Terry Pratchett", Name = "Guards! Guards!", Pages = 810 },
+            new List<int>() { 4, 6, 8, 2 },
+            new string[] { "Hello inside array" },
+            new Film()
+            {
+                Author = "Martin Scorsese", Name = "The Departed", Actors = new List<Actor>()
+                {
+                    new Actor() { Name = "Jack Nickolson", Birthdate = new DateTime(1937, 4, 22) },
+                    new Actor() { Name = "Leonardo DiCaprio", Birthdate = new DateTime(1974, 11, 11) },
+                    new Actor() { Name = "Matt Damon", Birthdate = new DateTime(1970, 8, 10) }
+                }
+            },
+            new Film()
+            {
+                Author = "Gus Van Sant", Name = "Good Will Hunting", Actors = new List<Actor>()
+                {
+                    new Actor() { Name = "Matt Damon", Birthdate = new DateTime(1970, 8, 10) },
+                    new Actor() { Name = "Robin Williams", Birthdate = new DateTime(1951, 8, 11) },
+                }
+            },
+            new Book() { Author = "Stephen King", Name = "Finders Keepers", Pages = 200 },
+            "Leonardo DiCaprio"
+        };
+
+        //#1
+        var allElements = data.Select(item => item switch
+        {
+            string str => str,
+            Book book => $"Book: {book.Name} by {book.Author}, {book.Pages} pages",
+            Film film => $"Film: {film.Name} by {film.Author}, " +
+                         $"Actors: {string.Join(", ", film.Actors.Select(a => $"{a.Name} - {a.Birthdate:yyyy-MM-dd}"))}",
+            IEnumerable<int> numbers => $"List of numbers: {string.Join(", ", numbers)}",
+            IEnumerable<string> strings => $"Array of strings: {string.Join(", ", strings)}",
+            _ => item?.ToString() ?? "Unknown"
+        });
+        
+        foreach (var element in allElements)
+        {
+            Console.WriteLine(element);
+        }
+        
+        Console.WriteLine("-----------------------------");
+        
+        //#2
+        var allDetails = data.SelectMany(item =>
+        {
+            if (item is Film film)
+            {
+                return new[]
+                {
+                    $"Film: {film.Name} by {film.Author}"
+                }.Concat(film.Actors.Select(actor => $"Actor: {actor.Name} - {actor.Birthdate:yyyy-MM-dd}"));
+            }
+
+            return Enumerable.Empty<string>();
+        });
+        
+        foreach (var element in allDetails)
+        {
+            Console.WriteLine(element);
+        }
+
+        Console.WriteLine(string.Join(", ", data.Last().ToString()));
+        
+        Console.WriteLine("-----------------------------");
+        
+        //#3
+        var actorsBornInAugust = data
+            .OfType<Film>()
+            .SelectMany(film => film.Actors) 
+            .Where(actor => actor.Birthdate.Month == 8)
+            .Distinct()
+            .Select(actor => $"{actor.Name} - {actor.Birthdate:yyyy-MM-dd}");
+
+        foreach (var actor in actorsBornInAugust)
+        {
+            Console.WriteLine(actor);
+        }
+        
+        Console.WriteLine("-----------------------------");
+        
+        //#4
+        var theOldestActors = data
+            .OfType<Film>()
+            .SelectMany(film => film.Actors) 
+            .Where(actor => actor.Birthdate.Year <= 1951)
+            .Select(actor => $"{actor.Name} - {actor.Birthdate:yyyy-MM-dd}");
+
+        foreach (var actor in theOldestActors)
+        {
+            Console.WriteLine(actor);
+        }
+        
+        Console.WriteLine("-----------------------------");
+        
+        //#5
+        var booksByAuthor = data
+            .OfType<Book>()
+            .GroupBy(book => book.Author) 
+            .Select(group => new { Author = group.Key, Count = group.Count() });
+        
+        int count = 0;
+        
+        foreach (var entry in booksByAuthor)
+        {
+            Console.WriteLine($"{entry.Author}: {entry.Count}");
+            count++;
+        }
+
+        Console.WriteLine("Total books: " + count);
+        
+        Console.WriteLine("---------------6-------------");
+        
+        //#6
+        booksByAuthor = data
+            .OfType<Book>()
+            .GroupBy(book => book.Author) 
+            .Select(group => new { Author = group.Key, Count = group.Count() });
+        
+        foreach (var entry in booksByAuthor)
+        {
+            Console.WriteLine($"{entry.Author}: {entry.Count}");
+        }
+
+        var filmsByAuthor = data
+            .OfType<Film>()
+            .GroupBy(book => book.Author) 
+            .Select(group => new { Author = group.Key, Count = group.Count() });
+
+        foreach (var entry in filmsByAuthor)
+        {
+            Console.WriteLine($"{entry.Author}: {entry.Count}");
+        }
     }
 }
